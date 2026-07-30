c608930d57de4fa55683cb6d685f4cec8afb9122
Tanishk Mangal <83267313+Tanishk192@users.noreply.github.com>
2026-07-31 02:59:55 +0530

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        int val = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(val!=nums[i]){
+                val = i;
+                break;
+            }
+
+            val++
+        }
+
+        return val;
+    }
+}
