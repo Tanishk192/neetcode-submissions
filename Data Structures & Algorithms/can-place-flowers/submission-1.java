f1db3ac869587f05c876fbe8757f8e444cf52ac8
Tanishk Mangal <83267313+Tanishk192@users.noreply.github.com>
2026-08-12 19:07:26 +0530

    Add: can-place-flowers - submission-1

---
+++ Data Structures & Algorithms/can-place-flowers/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n==0){
+            return true;
+        }
+        else if((flowerbed.length==1 && n>0) || (flowerbed.length==2 && n>0)){
+            return false;
+        }
+
+        if(n>0 && flowerbed[0]==0 && flowerbed[1]!=1){
+            flowerbed[0] = 1;
+            n--;
+        }
+
+        if(n>0 && flowerbed[flowerbed.length-1]==0 && flowerbed[flowerbed.length-2]!=1){
+            flowerbed[flowerbed.length-1] = 1;
+            n--;
+        }
+
+        for(int i = 1;i<flowerbed.length-1;i++){
+            if(n==0){
+                break;
+            }
+
+            if(flowerbed[i]!=1 && flowerbed[i-1]!=1 && flowerbed[i+1]!=1){
+                flowerbed[i] = 1;
+                n--;
+            } 
+        }
+
+        if(n==0){
+            return true;
+        }
+
+        return false;
+    }
+}
